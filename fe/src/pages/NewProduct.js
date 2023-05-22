680588a91988a69f10b591954c44c9c3c6bf0e8d
hansenf <hansenferiandy@gmail.com>
2023-05-22 22:06:35 +0700

    FE ecommerce

---
+++ fe/src/pages/NewProduct.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { Alert, Col, Container, Form, Row, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useCreateProductMutation } from "../services/appApi";
+import axios from "../axios";
+import "./NewProduct.css";
+
+function NewProduct() {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [images, setImages] = useState([]);
+    const [imgToRemove, setImgToRemove] = useState(null);
+    const navigate = useNavigate();
+    const [createProduct, { isError, error, isLoading, isSuccess }] = useCreateProductMutation();
+
+    function handleRemoveImg(imgObj) {
+        setImgToRemove(imgObj.public_id);
+        axios
+            .delete(`/images/${imgObj.public_id}/`)
+            .then((res) => {
+                setImgToRemove(null);
+                setImages((prev) => prev.filter((img) => img.public_id !== imgObj.public_id));
+            })
+            .catch((e) => console.log(e));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!name || !description || !price || !category || !images.length) {
+            return alert("Please fill out all the fields");
+        }
+        createProduct({ name, description, price, category, images }).then(({ data }) => {
+            if (data.length > 0) {
+                setTimeout(() => {
+                    navigate("/");
+                }, 1500);
+            }
+        });
+    }
+
+    function showWidget() {
+        console.log('ieu naon lieur', window.cloudinary)
+        const timestamp = Math.round(Date.now() / 1000)
+        const signature = window.cloudinary.utils.api_sign_request({ timestamp: timestamp, upload_preset: "konsultasi" }, "7Z-b1I5ArSHjvlO5qmclexn07UI")
+        const widget = window.cloudinary.createUploadWidget(
+            {
+                apiKey: "323545368373634",
+                cloudName: "temukandcloudinary",
+                uploadPreset: "konsultasi",
+                uploadSignature: signature
+            },
+            (error, result) => {
+                if (!error && result.event === "success") {
+                    setImages((prev) => [...prev, { url: result.info.url, public_id: result.info.public_id }]);
+                }
+            }
+        );
+        widget.open();
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col md={6} className="new-product__form--container">
+                    <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
+                        <h1 className="mt-4">Create a product</h1>
+                        {isSuccess && <Alert variant="success">Product created with succcess</Alert>}
+                        {isError && <Alert variant="danger">{error.data}</Alert>}
+                        <Form.Group className="mb-3">
+                            <Form.Label>Product name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter product name" value={name} required onChange={(e) => setName(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Product description</Form.Label>
+                            <Form.Control as="textarea" placeholder="Product description" style={{ height: "100px" }} value={description} required onChange={(e) => setDescription(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Price($)</Form.Label>
+                            <Form.Control type="number" placeholder="Price ($)" value={price} required onChange={(e) => setPrice(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" onChange={(e) => setCategory(e.target.value)}>
+                            <Form.Label>Category</Form.Label>
+                            <Form.Select>
+                                <option disabled selected>
+                                    -- Select One --
+                                </option>
+                                <option value="technology">technology</option>
+                                <option value="tablets">tablets</option>
+                                <option value="phones">phones</option>
+                                <option value="laptops">laptops</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Button type="button" onClick={showWidget}>
+                                Upload Images
+                            </Button>
+                            <div className="images-preview-container">
+                                {images.map((image) => (
+                                    <div className="image-preview">
+                                        <img src={image.url} />
+                                        {imgToRemove != image.public_id && <i className="fa fa-times-circle" onClick={() => handleRemoveImg(image)}></i>}
+                                    </div>
+                                ))}
+                            </div>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Button type="submit" disabled={isLoading || isSuccess}>
+                                Create Product
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </Col>
+                <Col md={6} className="new-product__image--container"></Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default NewProduct;
