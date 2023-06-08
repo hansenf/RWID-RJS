12aee3ea95d91d51661f012efa5594c9bd02c7c5
hansenf <hansenferiandy@gmail.com>
2023-06-08 22:43:23 +0700

    add alt to fix eslint issue

--- ecommerce/src/pages/shop/product.jsx
+++ ecommerce/src/pages/shop/product.jsx
@@ -9,7 +9,7 @@ export const Product = (props) => {
 
     return (
         <div className="product">
-            <img src={productImage} />
+            <img src={productImage} alt="products" />
             <div className="description">
                 <p>
                     <b>{productName}</b>
